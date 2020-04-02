60adfb09afdb83bff9c22fb525608a05aa0fe62a
Javier Gonzalez <javier.glez.martin@gmail.com>
2020-04-02 17:11:01 +0100

    Improve form in log in page

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Form } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
-import { AlertController } from '@ionic/angular';
 import { AlertsService } from 'src/app/services/alerts.service';
 
 @Component({
@@ -16,17 +14,15 @@ export class LoginPage implements OnInit {
 
   constructor( private router: Router,
                private authService: AuthService,
-               private alertController: AlertController,
                private alerts: AlertsService) { }
 
-  login(form: Form) {
+  login() {
     this.authService.login(this.model).subscribe(next => {
       this.router.navigate(['/menu/dashboard']);
     }, error => {
       this.alerts.presentAlert('Attention', 'Wrong password or email', '', 'OK');
+      this.model.password = '';
     });
-    this.model.email = '';
-    this.model.password = '';
   }
 
   ngOnInit() {
