dc6f8cc8049523f0140946076099397c67d49517
Javier Gonzalez <javier.glez.martin@gmail.com>
2020-04-02 17:38:06 +0100

    Centralize apiurl in environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'http://167.172.33.60/api/'
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
