7362174553238d98e5dd37aee8e65e36ab408b18
Javier Gonzalez <javier.glez.martin@gmail.com>
2020-04-04 18:15:11 +0100

    Add content to my-usages page

--- src/app/pages/my-usages/my-usages.page.ts
+++ src/app/pages/my-usages/my-usages.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { AlertsService } from 'src/app/services/alerts.service';
+import { UserDetailed } from 'src/app/models/user-detailed';
 
 @Component({
   selector: 'app-my-usages',
@@ -7,9 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyUsagesPage implements OnInit {
 
-  constructor() { }
+  user: Partial<UserDetailed> = {};
+  isData = false;
+
+  constructor(private userService: UserService,
+              private authService: AuthService,
+              private alertsService: AlertsService) { }
 
   ngOnInit() {
+    this.userService.getUser(this.authService.decodedToken.nameid).subscribe((user: UserDetailed) => {
+      this.user = user;
+      this.isData = true;
+    }, error => {
+      this.alertsService.presentToast('Error retrieving the user profile');
+    });
   }
 
 }
