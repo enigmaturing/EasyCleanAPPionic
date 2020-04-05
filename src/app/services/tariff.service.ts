3fd1ec55a25e4b9031670999cef15ab0b97cc3e9
Javier Gonzalez <javier.glez.martin@gmail.com>
2020-04-05 20:54:28 +0100

    Add tariffs service

---
+++ src/app/services/tariff.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Tariff } from '../models/tariff';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TariffService {
+
+  constructor(private http: HttpClient) { }
+
+  baseUrl = environment.apiUrl + 'tariffs/';
+
+  getTariffsOfMachineGroup(id: number): Observable<Tariff[]> {
+    return this.http.get<Tariff[]>(this.baseUrl + 'machinegroup/' + id);
+  }
+
+}
