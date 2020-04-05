490757672d0621458df042bbf04a08193647d4e9
Javier Gonzalez <javier.glez.martin@gmail.com>
2020-04-05 13:12:28 +0100

    Show machine groups in cards

--- src/app/pages/machines-list/machines-list.page.ts
+++ src/app/pages/machines-list/machines-list.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MachineService } from 'src/app/services/machine.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { AlertsService } from 'src/app/services/alerts.service';
+import { MachineGroup } from '../../models/machine-group';
 
 @Component({
   selector: 'app-machines-list',
@@ -7,9 +11,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MachinesListPage implements OnInit {
 
-  constructor() { }
+  machineGroups: Partial<MachineGroup>[] = [{}];
+
+  constructor(private machineService: MachineService,
+              private authService: AuthService,
+              private alertsService: AlertsService) { }
 
   ngOnInit() {
+    this.machineService.getMachineGroups().subscribe((machineGroups: MachineGroup[]) => {
+      this.machineGroups = machineGroups;
+    }, error => {
+      this.alertsService.presentToast('Error retrieving the avilable groups');
+    });
   }
 
 }
